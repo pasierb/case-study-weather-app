8799f1abedba25832e703493353c3e7904c586a4
pasierb <mpasierbski@gmail.com>
2018-04-21 17:29:37 +0200

    add bulma & DayOverview icon

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import DayOverview from './components/DayOverview';
 import { fetchDaily } from './utils/weatherbit';
 
-import './App.css';
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
       dailyData: [],
+      selected: undefined
     };
   }
 
@@ -21,17 +20,31 @@ class App extends Component {
       res.json()
     ).then(weatherbitData => {;
       this.setState({
-        dailyData: weatherbitData.data
+        dailyData: weatherbitData.data,
+        selected: weatherbitData.data[0]
       });
     })
   }
 
   render() {
+    const selected = this.state.selected;
+
     return (
       <div className="App">
-        {this.state.dailyData.map(({ temp, datetime, ts }) => (
-          <DayOverview key={ts} temp={temp} date={new Date(datetime)} />
-        ))}
+        <div className="columns">
+          <div className="column">
+            {selected && <DayOverview temp={selected.temp} date={new Date(selected.datetime)} iconCode={selected.weather.icon} />}
+          </div>
+          <div className="column">
+          </div>
+        </div>
+        <div className="columns">
+          {this.state.dailyData.map(({ temp, datetime, ts, weather }) => (
+            <div className="column" key={ts}>
+              <DayOverview temp={temp} date={new Date(datetime)} iconCode={weather.icon} />
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
--- src/components/DayOverview.js
+++ src/components/DayOverview.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+function weatherbitIconSrc(code) {
+  return ['https://www.weatherbit.io/static/img/icons/', code, '.png'].join('');
+}
+
 function DayOverview(props) {
-  return (<div>
-    {props.date.toLocaleString('en-US', { weekday: 'long' })}
-    {props.temp}
+
+  return (<div className="DayOverview">
+    <div className="has-text-centered">
+      {props.date.toLocaleString('en-US', { weekday: 'long' })}
+    </div>
+    <div className="has-text-centered">
+      <img src={weatherbitIconSrc(props.iconCode)} />
+    </div>
+    <div className="has-text-centered">
+      {props.temp}
+    </div>
   </div>);
 }
 
 DayOverview.propTypes = {
   temp: PropTypes.number.isRequired,
-  date: PropTypes.instanceOf(Date).isRequired
+  date: PropTypes.instanceOf(Date).isRequired,
+  iconCode: PropTypes.string
 }
 
 export default DayOverview;
